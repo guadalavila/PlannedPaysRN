6635c06b0877532f84d3d9b84e735e7ff2703995
Guadalupe <guadalupe.alavila@gmail.com>
2024-01-11 19:58:46 -0300

    mock categories

---
+++ src/utils/mock.ts
@@ -0,0 +1,9 @@
+export const CATEGORIES = [
+    { _id: 1, label: 'Comida' },
+    { _id: 2, label: 'Transporte' },
+    { _id: 3, label: 'Casa' },
+    { _id: 4, label: 'Salud' },
+    { _id: 5, label: 'Restaurante' },
+    { _id: 6, label: 'Ropa' },
+    { _id: 7, label: 'Facturas' },
+];
