b763e9048ec13d0f9e2e80d00bf9d7cc72d269d1
Guadalupe <guadalupe.alavila@gmail.com>
2024-02-05 21:40:28 -0300

    complete setting screen

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -3,20 +3,56 @@ import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { StyleSheet, View } from 'react-native';
 import { DrawerStackList } from '~navigations/types';
 import Container from '~components/Container';
-import PaletteColor from '~components/PaletteColor';
+import useAuth from '~hooks/useAuth';
+import Input from '~components/Input';
+import Text from '~components/Text';
+import { colors } from '~utils/colors';
+import { spacing } from '~utils/spacing';
+import { typography } from '~utils/typography';
 
 interface Props extends NativeStackScreenProps<DrawerStackList, 'SettingsScreen'> {}
 
 const SettingsScreen = ({}: Props) => {
+    const { user } = useAuth();
     return (
         <Container>
-            <View>
-                <PaletteColor />
+            <View style={styles.avatar}>
+                <Text style={styles.textAvatar}>
+                    {user.name.charAt(0).toUpperCase()}
+                    {user.lastName.charAt(0).toUpperCase()}
+                </Text>
             </View>
+            <Input value={user.name} placeholder='Nombre' keyboardType='default' onChangeText={(value) => {}} />
+            <Input value={user.lastName} placeholder='Nombre' keyboardType='default' onChangeText={(value) => {}} />
+            <Input
+                editable={false}
+                value={user.email}
+                placeholder='Nombre'
+                keyboardType='default'
+                onChangeText={(value) => {}}
+            />
+            {/* <PaletteColor  /> */}
         </Container>
     );
 };
 
 export default SettingsScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    avatar: {
+        marginVertical: spacing.XXL,
+        alignSelf: 'center',
+        width: 100,
+        height: 100,
+        borderRadius: 100,
+        backgroundColor: colors.light.primary,
+        position: 'relative',
+        justifyContent: 'center',
+    },
+    textAvatar: {
+        color: colors.light.white,
+        fontWeight: 'bold',
+        fontSize: typography.size.XXXL,
+        alignSelf: 'center',
+    },
+});
