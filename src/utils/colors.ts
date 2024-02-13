d3efc07ee453e486fc4977f176c7c7004a91a898
Guadalupe <guadalupe.alavila@gmail.com>
2024-02-12 22:16:48 -0300

    change colors

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -1,23 +1,33 @@
 const common = {
-    primary: '#181d30',
-    primaryDark: '#004c8f',
+    // primary: '#c5f277',
+    // primaryDark: '#B3D578',
+    primary: '#FCAB10',
+    primaryDark: '#FCAB10',
     secondary: '#4CE364',
     success: '#4CE364',
     tertiary: '#FFC700',
-    background: '#EFEFEF',
     white: '#FFFFFF',
     black: '#000000',
     separator: '#D1D1D1',
-    textPrimary: '#000000',
     grey: '#797979',
     error: '#DD0101',
 };
 
 const light = {
     ...common,
+    background: '#EFEFEF',
+    bgInput: '#FFFFFF',
+    textInput: '#434343',
+    bgCard: '#FFFFFF',
+    textPrimary: '#000000',
 };
 
 const dark = {
     ...common,
+    background: '#181818',
+    bgInput: '#252525',
+    textInput: '#FAFAFA',
+    bgCard: '#252525',
+    textPrimary: '#FAFAFA',
 };
 export const colors = { light, dark };
