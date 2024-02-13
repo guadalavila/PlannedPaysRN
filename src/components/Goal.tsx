df2f4c3ebd70a16f118caa96888e5402f0fb2455
Guadalupe <guadalupe.alavila@gmail.com>
2024-02-12 22:16:24 -0300

    ref Goal Component

--- src/components/Goal.tsx
+++ src/components/Goal.tsx
@@ -7,15 +7,19 @@ import { typography } from '~utils/typography';
 import Title from './Title';
 import { GlobalStyles } from '~utils/styles';
 import ProgressBar from './ProgressBar';
+import Text from './Text';
+import { spacing } from '~utils/spacing';
 
 interface IGoalProps {
     icon: string;
     title: string;
-    percentage: number;
+    percentage?: number;
     color?: string;
+    amountTotal?: number;
+    saved?: number;
 }
 
-const Goal = ({ icon, title, percentage, color = colors.light.primary }: IGoalProps) => {
+const Goal = ({ icon, title, percentage = 0, color = colors.light.primaryDark, amountTotal, saved }: IGoalProps) => {
     return (
         <Card>
             <View style={[styles.avatar, { backgroundColor: color }]}>
@@ -23,6 +27,10 @@ const Goal = ({ icon, title, percentage, color = colors.light.primary }: IGoalPr
             </View>
             <Title style={GlobalStyles.alignSelf} text={title} size={typography.size.L} />
             <ProgressBar label='' percent={percentage} withCard={false} />
+            <View style={[GlobalStyles.rowBetween, styles.containerAmount]}>
+                {saved && <Text>Guardado: $20000</Text>}
+                {amountTotal && <Text>Objetivo: {amountTotal}</Text>}
+            </View>
         </Card>
     );
 };
@@ -36,5 +44,8 @@ const styles = StyleSheet.create({
         borderRadius: 45,
         alignSelf: 'center',
         justifyContent: 'center',
+    },
+    containerAmount: {
+        paddingVertical: spacing.S,
     },
 });
