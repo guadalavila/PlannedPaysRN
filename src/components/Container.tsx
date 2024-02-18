3b8940cdd9a2e44303cd00a012c798b520b52a08
Guadalupe <guadalupe.alavila@gmail.com>
2024-02-17 23:16:22 -0300

    add loading

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -3,6 +3,8 @@ import { KeyboardAvoidingView, Platform, StatusBar, StyleSheet, View } from 'rea
 import { ThemeContext } from '~contexts/ThemeContext';
 import { GlobalStyles } from '~utils/styles';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import Loading from './Loading';
+import { StateContext } from '~contexts/StateContext';
 
 interface IContainerProps {
     children: React.ReactNode;
@@ -14,13 +16,14 @@ const Container = ({ children }: IContainerProps) => {
         theme,
     } = useContext(ThemeContext);
     const { top, bottom } = useSafeAreaInsets();
+    const { loading } = useContext(StateContext);
 
     return (
         <>
             <StatusBar translucent barStyle={'light-content'} />
             <View style={[styles.container, { backgroundColor: colors.background, paddingBottom: bottom }]}>
                 <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={{ flex: 1 }}>
-                    <View style={GlobalStyles.flex1}>{children}</View>
+                    <View style={GlobalStyles.flex1}>{loading ? <Loading /> : children}</View>
                 </KeyboardAvoidingView>
             </View>
         </>
--- src/components/ContainerSafe.tsx
+++ src/components/ContainerSafe.tsx
@@ -3,6 +3,8 @@ import { KeyboardAvoidingView, Platform, StatusBar, StyleSheet, View } from 'rea
 import { ThemeContext } from '~contexts/ThemeContext';
 import { GlobalStyles } from '~utils/styles';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import Loading from './Loading';
+import { StateContext } from '~contexts/StateContext';
 
 interface IContainerSafeProps {
     children: React.ReactNode;
@@ -13,6 +15,8 @@ const ContainerSafe = ({ children }: IContainerSafeProps) => {
         theme,
     } = useContext(ThemeContext);
     const { top, bottom } = useSafeAreaInsets();
+    const { loading } = useContext(StateContext);
+
     return (
         <>
             <StatusBar translucent barStyle={theme === 'dark' ? 'light-content' : 'dark-content'} />
@@ -22,7 +26,7 @@ const ContainerSafe = ({ children }: IContainerSafeProps) => {
                     { backgroundColor: colors.background, paddingBottom: bottom, paddingTop: top },
                 ]}>
                 <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={{ flex: 1 }}>
-                    <View style={GlobalStyles.flex1}>{children}</View>
+                    <View style={GlobalStyles.flex1}>{loading ? <Loading /> : children}</View>
                 </KeyboardAvoidingView>
             </View>
         </>
