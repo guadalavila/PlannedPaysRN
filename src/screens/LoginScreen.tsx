6b86e56da4b8c85d68e57a22a243e2cfc38c6262
Guadalupe <guadalupe.alavila@gmail.com>
2024-02-17 23:16:00 -0300

    LoginScreen

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,16 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { Button, Text, View } from 'react-native';
 import { RootStackLogoutParamList } from '~navigations/types';
+import Text from '~components/Text';
+import ContainerSafe from '~components/ContainerSafe';
+import { StyleSheet } from 'react-native';
+import { View } from 'react-native';
+import Input from '~components/Input';
+import useForm from '~hooks/useForm';
+import Button from '~components/Button';
+import useAuth from '~hooks/useAuth';
+import TextError from '~components/TextError';
+// import auth from '@react-native-firebase/auth';
 
 interface Props extends NativeStackScreenProps<RootStackLogoutParamList, 'LoginScreen'> {}
 
 const LoginScreen = ({ navigation }: Props) => {
+    const { fields, setFieldValue, handleSubmit, errors } = useForm('LoginForm', () => doLogin());
+    const { login } = useAuth();
+
+    const doLogin = () => {
+        login(fields.email, fields.password);
+    };
+
+    const signUp = () => {
+        navigation.navigate('SignUpScreen');
+    };
+
     return (
-        <View>
-            <Text>LogisdsnScreen</Text>
-        </View>
+        <ContainerSafe>
+            <View style={styles.container}>
+                <Input
+                    value={fields.email}
+                    placeholder='Email'
+                    keyboardType='email-address'
+                    onChangeText={(value) => setFieldValue('email', value.trim().toLowerCase())}
+                />
+                {errors.email && <TextError text={errors.email} />}
+                <Input
+                    value={fields.password}
+                    placeholder='Contraseña'
+                    keyboardType='default'
+                    secureTextEntry
+                    onChangeText={(value) => setFieldValue('password', value)}
+                />
+                {errors.password && <TextError text={errors.password} />}
+                <Button title='¿No tenes cuenta? Registrate' onPress={signUp} outlined />
+                <Button title='Ingresar' onPress={handleSubmit} />
+            </View>
+        </ContainerSafe>
     );
 };
 
 export default LoginScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'transparent',
+        flex: 1,
+    },
+});
